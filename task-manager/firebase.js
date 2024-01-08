260e5b33c3f8170a3dd4f53040b3ee710e953997
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-08 18:00:05 -0500

    Implement firebase

---
+++ task-manager/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBDMAUEmedVQ0YNdeySQILO9H-AAKZ8iOI",
+    authDomain: "task-manager-448ed.firebaseapp.com",
+    projectId: "task-manager-448ed",
+    storageBucket: "task-manager-448ed.appspot.com",
+    messagingSenderId: "95795147103",
+    appId: "1:95795147103:web:7bdb5b2efbd58a6e4cc6ea",
+    measurementId: "G-SJF98S2MNN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      <h1>Task Management</h1>
     </>
   )
 }
