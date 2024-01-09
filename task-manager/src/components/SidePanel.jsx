6416c2684fa459aca74bc2a2e7f56258586af16f
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-09 09:21:31 -0500

    Complete first implementation of the side panel

--- task-manager/src/components/SidePanel.jsx
+++ task-manager/src/components/SidePanel.jsx
@@ -1,7 +1,7 @@
 import styles from "../assets/stylesheets/SidePanel.module.css"
 import boardImg from "../assets/icon-board.svg"
 import hideSidebarImg from "../assets/icon-hide-sidebar.svg"
-
+import data from "../data.json"
 export default function SidePanel() {
     return (
         <section>
@@ -9,22 +9,20 @@ export default function SidePanel() {
                 <h1>kanban</h1>
             </div>
             <div className={styles.panelContent}>
-                <h4>ALL BOARDS ()</h4>
-                <div className={styles.panelItem + ' ' + styles.panelItemSelected}>
-                    <img src={boardImg} alt="" />
-                    <p>Platform Launch</p>
-                </div>
-                <div className={styles.panelItem}>
-                    <img src={boardImg} alt="" />
-                    <p>Marketing Plan</p>
-                </div>
+                <h4>ALL BOARDS ({data.boards.length})</h4>
+                {data.boards.map((board, index) => (
+                    <div key={index} className={styles.panelItem}>
+                        <img src={boardImg} alt=""/>
+                        <p>{board.name}</p>
+                    </div>
+                ))}
                 <div className={styles.panelItem + ' ' + styles.panelItemNew}>
-                    <img src={boardImg} alt="" />
+                    <img src={boardImg} alt=""/>
                     <p>+ Create New Board</p>
                 </div>
             </div>
             <div className={styles.panelItem + ' ' + styles.panelFooterContent}>
-                <img src={hideSidebarImg} alt="" />
+                <img src={hideSidebarImg} alt=""/>
                 <p>Hide Sidebar</p>
             </div>
         </section>
