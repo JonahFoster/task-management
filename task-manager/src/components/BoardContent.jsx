ee16f161c2091641c4ddbc0df0d2ce83db621207
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-09 20:44:33 -0500

    Pull columns from firebase and display in BoardContent

--- task-manager/src/components/BoardContent.jsx
+++ task-manager/src/components/BoardContent.jsx
@@ -1,28 +1,60 @@
 import styles from "../assets/stylesheets/BoardContent.module.css"
+import { useEffect, useState, useContext } from 'react'
+import { BoardContext } from '../contexts/BoardContext.jsx'
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../../firebase.js"
 
 export default function BoardContent() {
-    let column = true;
+    const { chosenBoard } = useContext(BoardContext)
+    const [ columns, setColumns ] = useState([])
+
+    async function fetchBoardData() {
+        if (chosenBoard) {
+            const colQuerySnapshot = await getDocs(collection(db, "users", "defaultUser", "boards", chosenBoard.id, "columns"))
+            const fetchedColumns = []
+            console.log(colQuerySnapshot)
+            console.log(colQuerySnapshot.docs)
+            for (let doc of colQuerySnapshot.docs) {
+                const column = { id: doc.id, ...doc.data() }
+                const tasksSnapshot = await getDocs(collection(db, "users", "defaultUser", "boards", chosenBoard.id, "columns"), "tasks")
+                column.tasks = tasksSnapshot.docs.map(taskDoc => ({ id: taskDoc.id, ...taskDoc.data() }))
+                fetchedColumns.push(column)
+                console.log(fetchedColumns)
+            }
+            setColumns(fetchedColumns)
+        }
+    }
+
+    useEffect(() => {
+        fetchBoardData()
+    }, [chosenBoard])
+
+    const hasColumns = columns.length > 0
+
     return (
         <div>
-            {column ? (
+            {hasColumns ? (
                 <div className={styles.mainBoardContainer}>
-                    <div className={styles.columnContainer}>
-                        <div className={styles.columnHeader}>
-                            <div className={styles.columnHeaderCircle}></div>
-                            <p className={styles.columnHeaderTitle}>TODO (3)</p>
-                        </div>
-                        <div className={styles.tasksContainer}>
-                            <div className={styles.individualTaskContainer}>
-                                <h3 className={styles.taskTitle}>Build UI for onboarding flow</h3>
-                                <p className={styles.taskSubTitle}>0 of 3 subtasks</p>
+                    {columns.map((column, index) => (
+                        <div key={index} className={styles.columnContainer}>
+                            <div className={styles.columnHeader}>
+                                <div className={styles.columnHeaderCircle}></div>
+                                <p className={styles.columnHeaderTitle}>{column.name} ({column.tasks.length})</p>
+                            </div>
+                            <div className={styles.tasksContainer}>
+                                <div className={styles.individualTaskContainer}>
+                                    <h3 className={styles.taskTitle}>Build UI for onboarding flow</h3>
+                                    <p className={styles.taskSubTitle}>0 of 3 subtasks</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             ) : (
                 <div className={styles.noColumnsMainBoardContainer}>
                     <div className={styles.noColumnsContainer}>
-                        <h2 className={styles.noColumnText}>This board is empty. Create a new column to get started.</h2>
+                        <h2 className={styles.noColumnText}>This board is empty. Create a new column to get
+                            started.</h2>
                         <button className={styles.noColumnButton}>+ Add New Column</button>
                     </div>
                 </div>
