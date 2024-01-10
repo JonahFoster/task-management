b9488afa85321fde4d7bf1c95191329ef6ba54b3
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-09 19:39:35 -0500

    Implement board container styles

--- task-manager/src/components/BoardContent.jsx
+++ task-manager/src/components/BoardContent.jsx
@@ -1,13 +1,32 @@
 import styles from "../assets/stylesheets/BoardContent.module.css"
 
 export default function BoardContent() {
+    let column = true;
     return (
-        <div className={styles.mainBoardContainer}>
-            <div className={styles.noColumnsContainer}>
-                <h2 className={styles.noColumnText}>This board is empty. Create a new column to get started.</h2>
-                <button className={styles.noColumnButton}>+ Add New Column</button>
-            </div>
+        <div>
+            {column ? (
+                <div className={styles.mainBoardContainer}>
+                    <div className={styles.columnContainer}>
+                        <div className={styles.columnHeader}>
+                            <div className={styles.columnHeaderCircle}></div>
+                            <p className={styles.columnHeaderTitle}>TODO (3)</p>
+                        </div>
+                        <div className={styles.tasksContainer}>
+                            <div className={styles.individualTaskContainer}>
+                                <h3 className={styles.taskTitle}>Build UI for onboarding flow</h3>
+                                <p className={styles.taskSubTitle}>0 of 3 subtasks</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.noColumnsMainBoardContainer}>
+                    <div className={styles.noColumnsContainer}>
+                        <h2 className={styles.noColumnText}>This board is empty. Create a new column to get started.</h2>
+                        <button className={styles.noColumnButton}>+ Add New Column</button>
+                    </div>
+                </div>
+            )}
         </div>
-
     )
 }
