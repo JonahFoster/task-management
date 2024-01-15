37627c813f9b4b3427f1fa7ee96a678f061b0e58
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-14 22:16:42 -0500

    Initial implementation of Firebase Auth

--- task-manager/firebase.js
+++ task-manager/firebase.js
@@ -2,6 +2,10 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +25,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const auth = getAuth(app)
 
-export { db }
+export { db, auth }
