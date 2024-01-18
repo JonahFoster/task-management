38c474a4f348df26483c184c84465583a304c417
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2024-01-18 14:38:00 -0500

    Initial homepage stylings

--- task-manager/src/components/Home.jsx
+++ task-manager/src/components/Home.jsx
@@ -1,23 +1,35 @@
-import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import Header from "./Header.jsx"
 import { useContext } from "react"
 import { ModalContext } from "../contexts/ModalContext.jsx"
+import styles from '../assets/stylesheets/Home.module.css'
 
 export default function Home() {
+
     const auth = getAuth()
     const navigate = useNavigate()
     const { showModal } = useContext(ModalContext)
 
-    function handleClick() {
+    function handleSignUp() {
         showModal('SignUpModal')
+    }
+
+    function handleLogin() {
+        showModal('LoginModal')
     }
 
     return (
         <div>
             <Header />
-            <button onClick={handleClick}></button>
+            <div className={styles.heroSection}>
+                <h1 className={styles.welcomeTitle}>start your <span className={styles.welcomeTitleJourni}>journi</span></h1>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.loginBtn} onClick={handleLogin}>Login</button>
+                    <button className={styles.signUpBtn} onClick={handleSignUp}>Sign Up</button>
+                </div>
+            </div>
+
         </div>
     )
 }
